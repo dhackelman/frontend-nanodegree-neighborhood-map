a3d1d21d8b695f825ffe7bdd9f8fa3b1ee707b6e
dhackelman <drew.hackelman@gmail.com>
2016-07-03 13:15:51 -0400

    Include app.js

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ function initMap() {
 
 function AppViewModel() {
     this.schoolName = "PWCS";
-    this.cmoName = "FirstlineSchools";
+    this.cmoName = "Firstline Schools";
 }
 
 // Activates knockout.js
