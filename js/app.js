654f3993b30c2ab57e4a0706b70dcf0251697b86
dhackelman <drew.hackelman@gmail.com>
2016-07-03 15:12:20 -0400

    Include 4 places + new labels and KO bindings

--- js/app.js
+++ js/app.js
@@ -2,15 +2,26 @@ var map;
 function initMap() {
     var mapDiv = document.getElementById('map');
     var map = new google.maps.Map(mapDiv, {
-      center: {lat:29.9726925,lng:-90.0803123},
+      center: {lat:35.994034,lng:-78.898621},
       zoom: 12
     });
   }
 
-function AppViewModel() {
-    this.schoolName = "PWCS";
-    this.cmoName = "Firstline Schools";
+
+function durhamBrewViewModel(name, location) {
+	this.name = name;
+    this.location = location;
 }
 
+var brews = ko.observableArray ([
+		{name: "FullSteam Brewery", location: "726 Rigsbee Ave, Durham, NC 27701"}, 
+		{name: "Bull City Burger and Brewery" , location: "107 E Parrish St, Durham, NC 27701"}, 
+		{name: "Ponysaurus Brewing Company" , location: "219 Hood St, Durham, NC 27701"}, 
+		{name: "Growler Grlz", location: "4810 Hope Valley Rd #110, Durham, NC 27707"}, 
+		{name: "Triangle Brewing Co.", location: "918 Pearl St, Durham, NC 27701" }
+	])
+
+
+
 // Activates knockout.js
-ko.applyBindings(new AppViewModel());
+ko.applyBindings(new durhamBrewViewModel());
