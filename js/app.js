c48005b72b1898bb4d2623fb37a6e3a747b5f770
dhackelman <drew.hackelman@gmail.com>
2016-07-03 19:49:59 -0400

    Add map marker on durham w/ info window

--- js/app.js
+++ js/app.js
@@ -1,11 +1,50 @@
-var map;
-function initMap() {
-    var mapDiv = document.getElementById('map');
-    var map = new google.maps.Map(mapDiv, {
-      center: {lat:35.994034,lng:-78.898621},
-      zoom: 12
-    });
-  }
+$(document).ready(function () {
+
+    function initMap() {
+
+        var location = new google.maps.LatLng(35.9940, -78.8986);
+
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+            center: location,
+            zoom: 12,
+            panControl: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+
+
+        var markerImage = 'marker.png';
+
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            icon: src="http://maps.google.com/mapfiles/ms/icons/red.png"   
+        });
+
+        var contentString = '<div class="info-window">' +
+                '<h3>Info Window Content</h3>' +
+                '<div class="info-content">' +
+                '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>' +
+                '</div>' +
+                '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 400
+        });
+
+        marker.addListener('click', function () {
+            infowindow.open(map, marker);
+        });
+
+
+    }
+
+
+    google.maps.event.addDomListener(window, 'load', initMap);
+
+});
 
 
 function durhamBrewViewModel(name, location) {
@@ -27,14 +66,9 @@ var brews = ko.observableArray ([
 ko.applyBindings(new durhamBrewViewModel());
 
 // Enabling btn to get user-location
+// Run using python -m SimpleHTTPServer 
+// navigate to http://localhost:8000/index.html
 
 
-function initialize(location) {
-    console.log(location);
-};
 
-$(document).ready(function()
-{
-	navigator.geolocation.getCurrentPosition(initialize);
 
-});
